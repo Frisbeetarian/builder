509f8f5994b9c0cfbc39887cb558ded96959dfce
Mohamad Sleiman Haidar <mhaidar@myki.com>
2023-02-27 18:46:37 +0300

    setup CRUD operations for projects resource

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,12 +1,34 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { UpdateCoffeeDto } from '../coffees/dto/update-coffee.dto';
 
 @ApiTags('projects')
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
+
+  @Get(':uuid')
+  findOne(@Param('uuid') uuid: number) {
+    return this.projectsService.findOne(uuid);
+  }
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.projectsService.findAll(paginationQuery);
+  }
 
   @Post()
   @HttpCode(HttpStatus.GONE)
@@ -14,5 +36,18 @@ export class ProjectsController {
     console.log('create project dto:', createProjectDto);
 
     return this.projectsService.create(createProjectDto);
+  }
+
+  @Patch(':uuid')
+  update(
+    @Param('uuid') uuid: string,
+    @Body() updateCoffeeDto: UpdateCoffeeDto,
+  ) {
+    return this.projectsService.update(uuid, updateCoffeeDto);
+  }
+
+  @Delete(':uuid')
+  remove(@Param('uuid') uuid: string) {
+    return this.projectsService.remove(uuid);
   }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Project } from './entities/project.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { Document } from '../documents/entities/document.entity';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { Coffee } from '../coffees/entities/project.entity';
+import { UpdateCoffeeDto } from '../coffees/dto/update-coffee.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
 export class ProjectsService {
@@ -17,6 +21,28 @@ export class ProjectsService {
     private readonly dataSource: DataSource,
   ) {}
 
+  async findOne(uuid: string) {
+    const project = await this.projectsRepository.findOne({
+      where: { uuid: uuid },
+      relations: ['documents'],
+    });
+
+    if (!project) {
+      throw new NotFoundException(`Project #${uuid} not found`);
+    }
+
+    return project;
+  }
+
+  findAll(paginationQuery: PaginationQueryDto): Promise<Project[]> {
+    const { limit, offset } = paginationQuery;
+    return this.projectsRepository.find({
+      relations: ['documents'],
+      skip: offset,
+      take: limit,
+    });
+  }
+
   async create(createProjectDto: CreateProjectDto) {
     console.log('create project dto:', createProjectDto);
     const project = this.projectsRepository.create({
@@ -25,6 +51,33 @@ export class ProjectsService {
     });
 
     return this.projectsRepository.save(project);
+  }
+
+  async update(uuid: string, updateProjectDto: UpdateProjectDto) {
+    const documents =
+      updateProjectDto.documents &&
+      (await Promise.all(
+        updateProjectDto.documents.map((document) =>
+          this.preloadDocumentByUuid(document.uuid),
+        ),
+      ));
+
+    const project = await this.projectsRepository.preload({
+      uuid,
+      ...updateProjectDto,
+      documents,
+    });
+
+    if (!project) {
+      throw new NotFoundException(`Project #${uuid} not found`);
+    }
+
+    return this.projectsRepository.save(project);
+  }
+
+  async remove(uuid: string) {
+    const project = await this.findOne(uuid);
+    return this.projectsRepository.remove(project);
   }
 
   private async preloadDocumentByUuid(uuid: string): Promise<Document> {
